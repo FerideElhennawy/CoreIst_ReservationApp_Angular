cf63969c40f64554f5b6dd3a4b9d80d23630d4d5
Feride Elhennawy <40718869+FerideElhennawy@users.noreply.github.com>
2023-04-28 20:32:21 +0300

    Add project files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'facilities', loadChildren: () => import('./modules/facilities/facilities.module').then(m => m.FacilitiesModule) }, 
+  { path: 'instruments', loadChildren: () => import('./modules/instruments/instruments.module').then(a => a.InstrumentsModule)},
+  { path: 'reservations', loadChildren: () => import('./modules/reservations/reservations.module').then(m => m.ReservationsModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment.development';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Session2Angular';
+  title = 'CoreIst_ReservationApp_Angular';
+
+  constructor(){
+    console.log(environment);
+  }
 }
